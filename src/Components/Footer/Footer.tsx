c94a73947c389b7f42a75a4533987e52564faa9b
Álvaro Sebastián <122423685+als3bas@users.noreply.github.com>
2023-10-03 02:37:57 -0300

    Update Footer.tsx

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -9,7 +9,7 @@ const listData = (data: Links[]) => (
         <a
           target="_blank"
           href={l.url}
-          rel="noreferrer"
+          rel={l?.rel || "noreferrer"}
         >{`${l.emoji} ${l.name}`}</a>
       </li>
     ))}
